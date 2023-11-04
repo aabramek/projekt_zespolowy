7432baf75cd9b43e184eb4af3247751aec7e56d2
aabramek <aa.bramek2013@gmail.com>
2023-11-04 22:19:22 +0100

    zmiany w sterowniku do mcp23017

--- stacja_pomiarowa/firmware/components/ic_driver/include/mcp23017.h
+++ stacja_pomiarowa/firmware/components/ic_driver/include/mcp23017.h
@@ -4,13 +4,40 @@
 #include <inttypes.h>
 
 #include "esp_err.h"
+#include "driver/i2c.h"
 
-#define IODIRA	0x00
-#define IODIRB	0x01
-#define GPIOA	0x12
-#define GPIOB	0x13
+#define IODIRA 0
+#define IODIRB 1
+#define IPOLA 2
+#define IPOLB 3
+#define GPINTENA 4
+#define GPINTENB 5
+#define DEFVALA 6
+#define DEFVALB 7
+#define INTCONA 8
+#define INTCONB 9
+#define IOCON 10
+#define GPPUA 11
+#define GPPUB 12
+#define INTFA 13
+#define INTFB 14
+#define INTCAPA 15
+#define INTCAPB 16
+#define GPIOA 17
+#define GPIOB 18
+#define OLATA 19
+#define OLATB 20
 
-esp_err_t MCP23017_Write(uint8_t device_addr, uint8_t register_addr, uint8_t value);
-esp_err_t MCP23017_Init(uint8_t device_addr, uint32_t sda_pin, uint32_t scl_pin);
+extern uint8_t MCP23017_REGISTER_MAP_BANK[21];
+extern uint8_t MCP23017_REGISTER_MAP_NON_BANK[21];
+
+typedef struct
+{
+	uint8_t address;
+	i2c_port_t i2c_port;
+}
+MCP23017_t;
+
+esp_err_t MCP23017_Write(MCP23017_t *device, uint8_t register_address, uint8_t value);
 
 #endif
--- stacja_pomiarowa/firmware/components/ic_driver/mcp23017.c
+++ stacja_pomiarowa/firmware/components/ic_driver/mcp23017.c
@@ -4,49 +4,23 @@
 #include "driver/i2c.h"
 #include "esp_check.h"
 
-esp_err_t MCP23017_Write(uint8_t device_addr, uint8_t register_addr, uint8_t value)
+uint8_t MCP23017_REGISTER_MAP_BANK[21] = {
+	0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15
+};
+
+uint8_t MCP23017_REGISTER_MAP_NON_BANK[21] = {
+	0x0, 0x10, 0x1, 0x11, 0x2, 0x12, 0x3, 0x13, 0x4, 0x14, 0x5, 0x6, 0x16, 0x7, 0x17, 0x8, 0x18, 0x9, 0x19, 0xa, 0x1a
+};
+
+esp_err_t MCP23017_Write(MCP23017_t *device, uint8_t register_address, uint8_t value)
 {
-	uint8_t buf[] = {register_addr, value};
-	return i2c_master_write_to_device(I2C_NUM_0, device_addr, buf, 2, pdMS_TO_TICKS(100));
-}
-
-esp_err_t MCP23017_Init(uint8_t device_addr, uint32_t sda_pin, uint32_t scl_pin)
-{
-	const char* TAG = "MCP23017_Init";
-
-	esp_err_t status;
-
-	i2c_config_t i2c_config = {
-		.mode = I2C_MODE_MASTER,
-		.sda_io_num = sda_pin,
-		.scl_io_num = scl_pin,
-		.sda_pullup_en = false,
-		.scl_pullup_en = false,
-		.master.clk_speed = 50000,
-		.clk_flags = 0
-	};
-
-	status = i2c_param_config(I2C_NUM_0, &i2c_config);
-	if (status != ESP_OK)
-	{
-		ESP_LOGE(TAG, "Cannot configure i2c peripheral");
-		goto end;
+	const char* TAG = "MCP23017_Write";
+	uint8_t buf[] = {register_address, value};
+	esp_err_t retval = ESP_ERR_TIMEOUT;
+	retval = i2c_master_write_to_device(device->i2c_port, device->address, buf, 2, pdMS_TO_TICKS(15));
+	while (retval == ESP_ERR_TIMEOUT) {
+		ESP_LOGW(TAG, "I2C busy...");
+		retval = i2c_master_write_to_device(device->i2c_port, device->address, buf, 2, pdMS_TO_TICKS(15));
 	}
-
-	status = i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
-	if (status != ESP_OK)
-	{
-		ESP_LOGE(TAG, "Cannot install i2c driver");
-		goto end;
-	}
-
-	status = MCP23017_Write(device_addr, 0x0A, 0x20);
-	if (status != ESP_OK)
-	{
-		ESP_LOGE(TAG, "Cannot send data to ic");
-		goto end;
-	}
-
-	end:
-	return status;
+	return retval;
 }
